245d6b370c46b8c7d921df86dc389ed449907d15
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2016-01-30 00:02:31 -0300

    Initial commit

---
+++ Money.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+
+namespace DotnetCoreNUnit
+{
+  public class Money : ValueObject<Money>
+  {
+    protected readonly decimal Value;
+
+    public Money() : this(0m)
+    {
+    }
+
+    public Money(decimal value)
+    {
+      Value = value;
+    }
+
+    public Money Add(Money money)
+    {
+      return new Money(Value + money.Value);
+    }
+
+    public Money Subtract(Money money)
+    {
+      if (money.Value > Value)
+        return new Money();
+        
+      return new Money(Value - money.Value);
+    }
+
+    protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
+    {
+      return new List<object>() { Value };
+    }
+
+    public static Money operator + (Money left, Money right)
+    {
+      return new Money(left.Value + right.Value);
+    }
+
+    public static Money operator - (Money left, Money right)
+    {
+      return new Money(left.Value - right.Value);
+    }
+  }
+}
---
+++ MoneyTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace DotnetCoreNUnit
+{
+  [TestFixture]
+  public class MoneyTests
+  {
+    [TestCase(2, 3, 5)]
+    [TestCase(4, 12, 16)]
+    [TestCase(0, 3790, 3790)]
+    public void AddingSomeMoney(decimal value1, decimal value2, decimal expected)
+    {
+      var m = new Money(value1);
+      var m2 = new Money(value2);
+      Assert.That(new Money(expected), Is.EqualTo(m.Add(m2)));
+    }
+
+    [TestCase(8, 3, 5)]
+    [TestCase(17, 0, 17)]
+    [TestCase(3, 8, 0)]
+    public void SubtractingSomeMoney(decimal value1, decimal value2, decimal expected)
+    {
+      var m = new Money(value1);
+      var m2 = new Money(value2);
+      Assert.That(new Money(expected), Is.EqualTo(m.Subtract(m2)));
+    }
+  }
+}
